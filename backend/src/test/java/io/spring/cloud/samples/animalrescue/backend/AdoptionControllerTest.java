6abdb8ac3e36dfb892f7e2bb0646509568429fdc
Alexey Nesterov <email@alexnesterov.com>
2020-11-16 10:55:11 +0000

    Cleanup

--- backend/src/test/java/io/spring/cloud/samples/animalrescue/backend/AdoptionControllerTest.java
+++ backend/src/test/java/io/spring/cloud/samples/animalrescue/backend/AdoptionControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.mockito.ArgumentMatchers.eq;
